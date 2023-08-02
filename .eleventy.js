c023ce64212cf6d0b6f5854256f752589a944a47
Kraktoos <kraktoos@gmail.com>
2023-08-02 23:58:36 +0100

    Add compression/minification

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,9 @@
 const pluginRss = require("@11ty/eleventy-plugin-rss");
+const eleventyPluginFilesMinifier = require("@sherby/eleventy-plugin-files-minifier");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(pluginRss);
+  eleventyConfig.addPlugin(eleventyPluginFilesMinifier);
   return {
     dir: {
       input: "src",
